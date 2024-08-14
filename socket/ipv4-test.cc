0ffa1f850badcfd49ab75cb70dc5cd4c8c480b4e
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-08-14 16:21:23 -0600

    - Studying the ipv4 code

--- socket/ipv4-test.cc
+++ socket/ipv4-test.cc
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 
 #include "VSocket.h"
 #include "Socket.h"
@@ -18,10 +19,14 @@ int main( int argc, char * argv[] ) {
    VSocket * s;	
    char a[512];
 
-   s = new Socket( 's' );
-   s->Connect( osi, 80 );
-   s->Write(  request );
-   s->Read( a, 512 );
-   printf( "%s\n", a);
+   s = new Socket( 's' );  //Creates a Socket class, it's a subclase of VSocket
+
+   s->Connect( osi, 80 );  //Connects the socket to the server using the "osi" IP in the port 80, HTTP standart port
+
+   s->Write(  request );   //Sends a request to the server
+
+   s->Read( a, 512 );      //Gets the response of the server and store it on buffer "a" (512 bytes)
+
+   printf( "%s\n", a);     //Prints the response
 
 }
