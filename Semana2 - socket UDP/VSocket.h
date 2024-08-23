7a69141c2ead0e949ac6b6c2165c6bb00670f534
Laboratorio-3-5 <lab-3-5@lab-3-5-WS-16.ecci.ucr.ac.cr>
2024-08-23 10:08:49 -0600

    - Adding the week #1 work

---
+++ Semana2 - socket UDP/VSocket.h
@@ -0,0 +1,43 @@
+/**
+ *  Universidad de Costa Rica
+ *  ECCI
+ *  CI0123 Proyecto integrador de redes y sistemas operativos
+ *  2024-ii
+ *  Grupos: 3 y 5
+ *
+ * (versión Fedora)
+ *
+ **/
+
+
+#ifndef VSocket_h
+#define VSocket_h
+ 
+class VSocket {
+   public:
+       void CreateVSocket( char, bool=false );
+       void CreateVSocket( int );
+      ~VSocket();
+
+      void Close();
+      int MakeConnection( const char *, int );
+      virtual int Connect( const char *, int ) = 0;
+
+      virtual size_t Read( void *, size_t ) = 0;
+      virtual size_t Write( const void *, size_t ) = 0;
+      virtual size_t Write( const char * ) = 0;
+
+      int Bind( int );
+
+      size_t sendTo( const void *, size_t, void * );
+      size_t recvFrom( void *, size_t, void * );
+
+   protected:
+      int idSocket;	// Socket identifier
+      bool IPv6;	// Is IPv6 socket?
+      int port;		// Socket associated port
+        
+};
+
+#endif // VSocket_h
+
