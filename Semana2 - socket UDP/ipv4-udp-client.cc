4cd30cacd24091a944af3e107a57c2e25e83d597
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-08-24 14:47:25 -0600

    - Documenting code client

--- Semana2 - socket UDP/ipv4-udp-client.cc
+++ Semana2 - socket UDP/ipv4-udp-client.cc
@@ -21,35 +21,42 @@
 #define MAXLINE 1024 
 
 int main() {
-   VSocket * client;
-   int sockfd; 
-   int n, len; 
-   char buffer[MAXLINE]; 
-   char *hello = (char *) "Hello from CI0123 client"; 
-   struct sockaddr_in other;
+	VSocket * client; //	Creates a Vsocket
 
-   client = new Socket( 'd' );	// Creates an UDP socket: datagram
+	int sockfd;
+	int n, len;
+	char buffer[MAXLINE];
+	char *hello = (char *) "Hello from CI0123 client";
 
-   memset( &other, 0, sizeof( other ) ); 
-   
-   other.sin_family = AF_INET; 
-   other.sin_port = htons(PORT); 
-   n = inet_pton( AF_INET, "172.16.123.70", &other.sin_addr );	// IP address to test our client with a Python server on lab 3-5
-   if ( 1 != n ) {
-      printf( "Error converting from IP address\n" );
-      exit( 23 );
-   }
+	struct sockaddr_in other; // Specific for IPv4 connections
 
-   n = client->sendTo( (void *) hello, strlen( hello ), (void *) & other ); 
-   printf("Client: Hello message sent.\n"); 
-   
-   n = client->recvFrom( (void *) buffer, MAXLINE, (void *) & other );
-   buffer[n] = '\0'; 
-   printf("Client message received: %s\n", buffer); 
+	client = new Socket( 'd' );	// Creates an UDP socket: datagram "d"
 
-   client->Close(); 
+	memset( &other, 0, sizeof( other ) ); // Set the Struct sockaddr_in in 0, empty
 
-   return 0;
- 
-} 
+	other.sin_family = AF_INET; // Directions Family IPv4
 
+	other.sin_port = htons(PORT); //sin_port it's an atribute that stores the number of port in network format, htons means "Host TO Network Short".
+	
+	// 192.168.28.212 -> my apartment IP
+	n = inet_pton( AF_INET, "192.168.28.212", &other.sin_addr );	// IP address to test our client with a Python server on lab 3-5
+
+	if ( 1 != n ) {
+		printf( "Error converting from IP address\n" );
+		exit( 23 ); // Error to convert IP code -> "23"
+	}
+
+	n = client->sendTo( (void *) hello, strlen( hello ), (void *) & other ); // sendTo Method that we have to do
+
+	printf("Client: Hello message sent.\n");
+
+	n = client->recvFrom( (void *) buffer, MAXLINE, (void *) & other ); // recvFrom Method that we have to do
+
+	buffer[n] = '\0'; // Adds a null char at the end of the string
+
+	printf("Client message received: %s\n", buffer);
+
+	client->Close();
+
+	return 0;
+}
